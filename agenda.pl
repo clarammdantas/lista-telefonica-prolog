ca7f9a5ddfa0e8777b6a969a2822b058f4618b9e
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-01-22 20:11:33 -0300

    Adicionar e exibir funcionando

--- agenda.pl
+++ agenda.pl
@@ -1,10 +1,31 @@
 :-[mensagens].
 
 
-:- initialization(main).
+
+:- initialization(main).	
+
+% Método que imprime o contato e seu respctivo número.
+
+exibeContatos([]).
+exibeContatos([Head|Tail]):-
+		write('Nome: '),write(Head),nl,
+		contato(Head,Y),
+		write('Numero: '),write(Y),nl,nl,
+		exibeContatos(Tail).
+
+% Adiciona fato contato(NOME,NUMERO) a base de dados
+
+adicionaContato():-
+	read(NOME),nl,
+	read(NUMERO),
+	assertz(contato(NOME,NUMERO)).
 
 main:-
-exibeMenu(),
+adicionaContato(),
+findall(X, (contato(X,Y)),L),
+exibeContatos(L).
+
+
 halt(0).
 
 
