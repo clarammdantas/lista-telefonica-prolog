4deb8ef939b675df2d9e6637ceb99a464bb7c56b
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2018-01-23 16:16:48 -0300

    Menu funcionando (opções 1, 2 e 3 parcialmente)

--- agenda.pl
+++ agenda.pl
@@ -1,31 +1,51 @@
 :-[mensagens].
-
-
-
 :- initialization(main).	
 
-% Método que imprime o contato e seu respctivo número.
+%Menu principal.
 
-exibeContatos([]).
+executaMenu():-
+	exibeMenu(),
+	read(OPCAO),nl,
+	sw(OPCAO).
+	
+%Opcões Menu principal.
+
+sw(1):-
+    adicionaContato().
+sw(2):-
+	findall(X, (contato(X,Y)),L),
+	exibeContatos(L).
+sw(3):-
+	apagaContato().
+
+% Adiciona fato contato(NOME,NUMERO) a base de dados
+
+adicionaContato():-
+	write("Nome do Contato: "),read(NOME),nl,
+	write("Numero do Contato: "), read(NUMERO),
+	assertz(contato(NOME,NUMERO)),
+	executaMenu().
+	
+% Método que imprime os contatos e seus respctivos números.
+
+exibeContatos([]):-
+		executaMenu().
 exibeContatos([Head|Tail]):-
 		write('Nome: '),write(Head),nl,
 		contato(Head,Y),
 		write('Numero: '),write(Y),nl,nl,
 		exibeContatos(Tail).
 
-% Adiciona fato contato(NOME,NUMERO) a base de dados
+% Método que apaga fato contato(NOME, _) da base de dados.
+% FIXME: Programa quebra se tentar apagar um contato nao existente.
 
-adicionaContato():-
-	read(NOME),nl,
-	read(NUMERO),
-	assertz(contato(NOME,NUMERO)).
+apagaContato():-
+	write("Nome do Contato: "),read(NOME),nl,
+	retract(contato(NOME, X)),
+	executaMenu().
 
 main:-
-adicionaContato(),
-findall(X, (contato(X,Y)),L),
-exibeContatos(L).
-
-
+executaMenu().
 halt(0).
 
 
--- mensagens.pl
+++ mensagens.pl
@@ -1,11 +1,12 @@
 exibeMenu():-
 	write('1.Adicionar contato'),nl,	
 	write('2.Listar contatos'),nl,
-	write('3.Buscar contato'),nl,
-	write('4.Ordenar '),nl,
-	write('5.Chamar '),nl,
-	write('6.Favoritos'),nl,
-	write('7.Sair'),nl,
+	write('3.Apagar contato'),nl,
+	write('4.Buscar contato'),nl,
+	write('5.Ordenar '),nl,
+	write('6.Chamar '),nl,
+	write('7.Favoritos'),nl,
+	write('8.Sair'),nl,
 	write('Digite sua opcao: '),nl.
 	
 	
